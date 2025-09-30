57da9604ba8ca132c54d5f5e866c49388f65a7ac
Sungsu Ahn <sungsu_ahn@korea.ac.kr>
2025-09-30 10:09:56 +0000

    fix famfs.c code bug occured during merging

--- famfs-linux/fs/fuse/famfs.c
+++ famfs-linux/fs/fuse/famfs.c
@@ -1111,4 +1111,9 @@ famfs_fuse_mmap(struct file *file, struct vm_area_struct *vma)
 	file_accessed(file);
 	vma->vm_ops = &famfs_file_vm_ops;
 	// pr_info("[%s] DEBUG_MAPPING: vma->vm_ops=%p\n", __func__, vma->vm_ops);
-	// sungsu: disable hugepage support by using below line, but now just use VM_HUG
+	vm_flags_set(vma, VM_HUGEPAGE | VM_MIXEDMAP);
+	// pr_info("[%s]: vma->vm_flags=0x%lx\n", __func__, vma->vm_flags);
+	// pr_info("[%s]: vma->vm_file->f_mapping->nrpages=%lu\n", __func__,
+	// 	vma->vm_file->f_mapping->nrpages);
+	return 0;
+}

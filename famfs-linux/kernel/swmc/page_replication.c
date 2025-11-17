312652cbd6f44ed39176a3be4fc2f12fc3d52358
Sungsu Ahn <sungsu_ahn@korea.ac.kr>
2025-11-17 06:37:44 +0000

    change threshold to 2GB

--- famfs-linux/kernel/swmc/page_replication.c
+++ famfs-linux/kernel/swmc/page_replication.c
@@ -840,7 +840,7 @@ int create_page_replica(struct page *page_original, unsigned int order)
     size_t size = PAGE_SIZE << order; // Calculate size based on order
 
     long avail_pages = si_mem_available();
-    long threshold_pages =  (1UL << 30) >> PAGE_SHIFT; // 1GB threshold
+    long threshold_pages =  (2UL << 30) >> PAGE_SHIFT; // 2GB threshold
 
     if (avail_pages < threshold_pages) {
         return -ENOMEM;
